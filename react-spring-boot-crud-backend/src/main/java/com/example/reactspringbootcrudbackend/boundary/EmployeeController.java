ef3b03d9bda2539ab1019e5463c69bc652916a55
tutncrusoe <huydinhanhle@gmail.com>
2023-01-01 11:22:46 +0700

    add findById - updateEmployee

--- react-spring-boot-crud-backend/src/main/java/com/example/reactspringbootcrudbackend/boundary/EmployeeController.java
+++ react-spring-boot-crud-backend/src/main/java/com/example/reactspringbootcrudbackend/boundary/EmployeeController.java
@@ -1,8 +1,11 @@
 package com.example.reactspringbootcrudbackend.boundary;
 
 import com.example.reactspringbootcrudbackend.model.Employee;
+import com.example.reactspringbootcrudbackend.repository.EmployeeRepository;
 import com.example.reactspringbootcrudbackend.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +17,19 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     /*----------------------      GET     ----------------------*/
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeService.findById(id);
+        return ResponseEntity.ok(employee);
     }
 
     /*----------------------      POST     ----------------------*/
@@ -27,5 +38,17 @@ public class EmployeeController {
         return employeeService.save(employee);
     }
 
+    /*----------------------      PUT     ----------------------*/
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,
+                                                   @RequestBody Employee employeeDto) {
+        Employee existingEmployee = employeeService.findById(id);
+            existingEmployee.setFirstName(employeeDto.getFirstName());
+            existingEmployee.setLastName(employeeDto.getLastName());
+            existingEmployee.setEmailId(existingEmployee.getEmailId());
+
+            Employee updatedEmployee = employeeRepository.save(existingEmployee);
+            return ResponseEntity.ok(updatedEmployee);
+    }
 
 }
--- react-spring-boot-crud-backend/src/main/java/com/example/reactspringbootcrudbackend/service/EmployeeService.java
+++ react-spring-boot-crud-backend/src/main/java/com/example/reactspringbootcrudbackend/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.example.reactspringbootcrudbackend.service;
 
+import com.example.reactspringbootcrudbackend.exception.ResourceNotFoundException;
 import com.example.reactspringbootcrudbackend.model.Employee;
 import com.example.reactspringbootcrudbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,10 @@ public class EmployeeService {
     /*----------------------      SAVE     ----------------------*/
     public Employee save(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    public Employee findById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
     }
 }
